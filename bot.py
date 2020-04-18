228d03b00d9066f7ecd79e86eeddd0e1503f7574
br00klyn93 <bmclaury93@gmail.com>
2020-04-18 11:56:55 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,17 @@ from telegram.ext import CommandHandler
 import logging
 from telegram.ext import MessageHandler, Filters
 
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+from datetime import datetime
+
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+
 updater = Updater(token='1246775743:AAEifr_IQFT7VeSAl9waeyIA2ZoUGYBEOCA', use_context=True)
 
 dispatcher = updater.dispatcher
@@ -11,20 +22,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a black, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot Initialized.")
 
+@app.route('/')
+def main():
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
+    updater.start_polling()
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
     # update.message.text is how you get any text posted to the channel
     print(update.message.text)
 
-#Echos everything said in chat.
 
-# echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-# dispatcher.add_handler(echo_handler)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
