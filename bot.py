f2240e47702e2aa2e6d5a6f44888c443ec07d0dc
br00klyn93 <bmclaury93@gmail.com>
2020-04-18 12:00:08 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ def start(update, context):
 def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-
     updater.start_polling()
+    return 'Bot Initialized.'
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -39,3 +39,6 @@ def echo(update, context):
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=5000)
